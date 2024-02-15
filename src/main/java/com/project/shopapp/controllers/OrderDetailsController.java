e63cf1b9ca3b25be2bb8085944bd648730f8ad86
dat02092k1 <ngodat02092001@gmail.com>
2024-02-15 17:52:32 +0700

    feat: order, oderdetails module

---
+++ src/main/java/com/project/shopapp/controllers/OrderDetailsController.java
@@ -0,0 +1,51 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.dtos.OrderDetailsDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("{api.prefix}/order_details")
+public class OrderDetailsController {
+    @PostMapping("")
+    public ResponseEntity<?> createOrderDetail(
+            @Valid @RequestBody OrderDetailsDTO orderDetailsDTO,
+            BindingResult result
+    ) {
+        if (result.hasErrors()) {
+            List<String> errorMsg = result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+            return ResponseEntity.badRequest().body(errorMsg.toString());
+        }
+
+        return ResponseEntity.ok("Order details created " + orderDetailsDTO.getOrderId());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getOrderDetails(@Valid @PathVariable("id") Long id) {
+        return ResponseEntity.ok("Order details " + id);
+    }
+
+    // get list order details of an order
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<?> getOrderDetailsByOrderId(@Valid @PathVariable("orderId") Long orderId) {
+        return ResponseEntity.ok("Order details by order id " + orderId);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateOrderDetails(
+            @Valid @PathVariable("id") Long id,
+            @RequestBody OrderDetailsDTO orderDetailsDTO
+    ) {
+        return ResponseEntity.ok("updateOrderDetails with id " + id + " " + orderDetailsDTO);
+    }
+
+
+}
---
+++ src/main/java/com/project/shopapp/dtos/OrderDTO.java
@@ -0,0 +1,44 @@
+package com.project.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class OrderDTO {
+    @JsonProperty("user_id")
+    @Min(value = 1, message = "User id must be greater than 0")
+    private Long userId;
+
+    @JsonProperty("full_name")
+    private String fullName;
+
+    private String email;
+
+    @JsonProperty("phone_number")
+    @NotBlank(message = "Phone number is required")
+    @Min(value = 10, message = "Phone number must be at least 10 characters")
+    private String phoneNumber;
+
+    private String address;
+
+    private String note;
+
+    @JsonProperty("total_money")
+    @Min(value = 0, message = "Total money must be greater than 0")
+    private Float totalMoney;
+
+    @JsonProperty("shipping_method")
+    private String shippingMethod;
+
+    @JsonProperty("shipping_address")
+    private String shippingAddress;
+
+    @JsonProperty("payment_method")
+    private String paymentMethod;
+}
